952fd5c071f6afabb59c653284ab207573b9bad8
GarvitDadheech <garvit.23bcs10203@ms.sst.scaler.com>
2024-11-17 19:51:31 +0530

    add bg

--- app/page.tsx
+++ app/page.tsx
@@ -5,17 +5,19 @@ import { WalletDisconnectButton, WalletModalProvider, WalletMultiButton } from "
 
 export default function Home() {
   return (
-    <ConnectionProvider endpoint={"https://api.devnet.solana.com"}>
-      <WalletProvider wallets={[]} autoConnect>
-        <WalletModalProvider>
-          <WalletMultiButton/>
-          <WalletDisconnectButton/>
-      <div>
-        LaunchPlex
-        <TokenCard />
-      </div>
-      </WalletModalProvider>
-      </WalletProvider>
-    </ConnectionProvider>
+    <div className="bg-custom-bg min-h-screen bg-cover bg-center">
+      <ConnectionProvider endpoint={"https://api.devnet.solana.com"}>
+        <WalletProvider wallets={[]} autoConnect>
+          <WalletModalProvider>
+            <WalletMultiButton/>
+            <WalletDisconnectButton/>
+        <div>
+          LaunchPlex
+          <TokenCard />
+        </div>
+        </WalletModalProvider>
+        </WalletProvider>
+      </ConnectionProvider>
+    </div>
   );
 }
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,18 +1,15 @@
-import type { Config } from "tailwindcss";
-
 export default {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
-      colors: {
-        background: "var(--background)",
-        foreground: "var(--foreground)",
+      backgroundImage: {
+        'custom-bg': "url('/assets/5557528.jpg')",
       },
     },
   },
   plugins: [],
-} satisfies Config;
+};
